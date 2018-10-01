98c34b9c347ac081927d01ece7ff2652ef8db8c4
fishproduct <43560358+fishproduct@users.noreply.github.com>
2018-10-01 18:57:03 +0800

    Update a1.cpp

--- a1.cpp
+++ a1.cpp
@@ -17,14 +17,13 @@ int main(){
 	cout << "Input the number of target points:" << endl;
 	cin >> n;
 	square a[n];
-	square b[];
 	
 	cout << "Input " << n << "suqares' x, y, width, height:"<< endl;
 	
 	for (int i = 0; i < n; i = i + 1){
 		cin >> a[i].x >> a[i].y >> a[i].width >> a[i].height;
 		a[i].widthscale = round (a[i].width/2);
-		a[i].heightscale = round (a[j].height/2);
+		a[i].heightscale = round (a[i].height/2);
 		a[i].scale = round (a[i].widthscale / a[i].heightscale);
 		a[i].area = a[i].width * a[i].height;
 	}
@@ -32,8 +31,11 @@ int main(){
 	// gather the points which are closest to each other in x-axis
 	sort (a[0].x, a[n].x); 
 
+	int num = n;
+	square b[n];
 	// find the possible pairs
-	for (int j = 0, int m = 0; j < n; j = j + 1){
+	int m = 0;
+	for (int j = 0; j < n; j = j + 1){
 		for (int k = j + 1; k < n; k = k + 1){
 			if (a[j].y == a[k].y && a[j].scale == a[k].scale)
 			{	
@@ -57,9 +59,9 @@ int main(){
 			cout << "Target found: " << endl;
 			cout << b[0].x << b[0].y << b[0].width << b[0].height << endl;
 			cout << b[1].x << b[1].y << b[1].width << b[1].height << endl;
+		}
 	if (pair > 1)
 	{ 
-		int num = 0;
 		double max = b[num].area;
 		for (int i = 2; i < pair * 2; i = i + 2){
 			if (b[i].area > max){
