c9235b1f438adc3bbcd9a9cd5b0ad9fe084ff9c4
coconutgit <43692301+coconutgit@users.noreply.github.com>
2018-10-01 23:47:55 +0800

    Update rec.cpp

--- rec.cpp
+++ rec.cpp
@@ -17,3 +17,22 @@ void rec::getNumber(){
 
 }
 
+bool judgeSize(rec a, rec b){
+
+     if (a.y==b.y && a.width==b.width && (a.height-b.height)<5 && (a.height-b.height)>-5)
+
+        return true;
+     else return false;
+
+}
+
+int getDistance(rec a, rec b){
+
+     int x;
+
+     if (a.x>=b.x) x=a.x-b.x;
+     else x=b.x-a.x;
+
+     return x;
+}
+
