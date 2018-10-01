421fb595f1c06d25a145ba3511d8c3f2e1b0b8e8
coconutgit <43692301+coconutgit@users.noreply.github.com>
2018-10-01 17:47:19 +0800

    Add files via upload

---
+++ rec.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+#include <cmath>
+#include "rec.h"
+using namespace std;
+
+void rec::setNumber(){
+
+     cin >> x >> y>> width >> height;
+
+
+}
+
+void rec::getNumber(){
+
+     cout << "("<<x <<","<< y <<","<< width<<"," << height<<")";
+
+}
+
---
+++ rec.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+class rec{
+
+  public:
+    int x;
+    int y;
+    double width;
+    double height;
+
+    void setNumber();
+    void getNumber();
+
+    void roundSize();
+
+};
