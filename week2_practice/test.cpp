491731642084fd6b9ed1dfb18f55f2d5728d25bf
GloRiation <ivanyiu2017@gmail.com>
2018-10-07 12:29:28 +0800

    implemented couting contour to check the correct threshold

--- week2_practice/test.cpp
+++ week2_practice/test.cpp
@@ -11,8 +11,8 @@ int main() {
 	string original_window_title, processed_window_title;   
     Mat image[2], image_copied[2],  image_gray[2];
 	Mat image_processed[2];
-	vector<vector<Point>> contour;
-	vector<Vec4i> hierarchy;
+    vector<vector<Point>> contour1, contour2;
+	vector<Vec4i> hierarchy1, hierarchy2;
 	image[0] = imread("Rune1.jpg");
     image[1] = imread("Rune2.png");
 	// Validation of the image
@@ -28,20 +28,14 @@ int main() {
 
 	// First convert it into grayscale picture
     for(int i = 0; i < 2; i++){ 
-	    cvtColor(image_copied[i], image_gray[i], CV_BGR2GRAY);
+        cvtColor(image_copied[i], image_gray[i], CV_BGR2GRAY);
 	    // then do thresholding to convert in into a binary picture
-	    threshold(image_gray[i], image_processed[i], 100, 255, THRESH_BINARY);
+	    threshold(image_gray[i], image_processed[i], 200, 200, THRESH_BINARY);
     }
-	// Finding contour, contours mean a closed curvei
-	/*findContours(image_processed, contour, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0));
-	
-    // First convert it into grayscale picture 
-	cvtColor(image_processed, image_gray, CV_BGR2GRAY);
-	// then do thresholding to convert in into a binary picture
-	threshold(image_gray, image_processed, 100, 255, THRESH_BINARY);
-	// Finding contour, contours mean a closed curvei
-	findContours(image_processed, contour, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0));
-
+    
+    // Finding contour, contours mean a closed curve
+    findContours(image_processed[0], contour1, hierarchy1, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0));
+    cout << "number of parent contours1 detected:" << contour1.size() << endl;
 /*	for (int i = 0; i < contour.size(); i++) {
 		drawContours(image_processed, contour, i, Scalar(0, 0, 255), 3, 8, hierarchy, 0, Point());
 	}*/
