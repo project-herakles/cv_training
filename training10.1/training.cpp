dee852ccddb38080505a5906a6a95767f7e6082f
neoys97 <neoys97@gmail.com>
2018-10-07 16:49:09 +0800

    contours?

--- training10.1/training.cpp
+++ training10.1/training.cpp
@@ -16,12 +16,21 @@ int main()
 	while(1)
 	{
 		//cap.set(CAP_PROP_FPS, 0.1);
-		Mat frame;
+		Mat frame, processedFrame;
 		cap >> frame;
 		if(frame.empty())
 			break;
+		cvtColor( frame, processedFrame, CV_BGR2GRAY );
+		threshold( processedFrame, processedFrame, 170, 255, 0 );
+		//fastNlMeansDenoising( processedFrame, processedFrame, 20 );
+		GaussianBlur( processedFrame, processedFrame, Size (11, 11), 0, 0 );
+		vector<vector<Point>> contours;
+		vector<Vec4i> hierarchy;
+		findContours ( processedFrame, contours, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_TC89_L1 );
+		drawContours( frame, contours, -1, Scalar(0, 255, 0), 1 );
 		imshow("Frame", frame);
-		char c=(char)waitKey(500);
+		char c=(char)waitKey(25);
+
 		if (c==27)
 			break;
 	}
