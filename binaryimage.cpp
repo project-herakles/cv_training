8845f985b699bb3bcf9fd0fccbd49c51af174ae2
linyaya <43580250+linyaya@users.noreply.github.com>
2018-10-08 18:04:30 +0800

    Create binaryimage.cpp

---
+++ binaryimage.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string>
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char** argv )
+{
+    if ( argc != 2 )
+    {
+       cout << "incorrect command"<< endl;
+    }
+
+    string copyName = argv[1];
+    Mat copyImage;
+    copyImage = imread(copyName,IMREAD_COLOR);
+
+    Mat grayImage,binaryImage;
+    cvtColor(copyImage, grayImage, COLOR_RGB2GRAY); //convert to a grayscale image (only by light)
+    // what is threshold value and max?????????
+    threshold(grayImage, binaryImage,1,128,THRESH_BINARY);
+    
+    int length= copyName.length();
+    string binaryName = copyName.substr(0,length-9) + "binary.jpg";
+    imwrite(binaryName,binaryImage);
+
+
+    return 0;
+
+}
