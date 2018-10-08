1cf3aa981b26a98f137ed28c8704ed780114ea34
GloRiation <ivanyiu2017@gmail.com>
2018-10-08 18:33:01 +0800

    successfully build a model to find out correct contour with ~90% accuracy, but still not crop and implement them in a separate window

--- assignment_2/main.cpp
+++ assignment_2/main.cpp
@@ -9,13 +9,13 @@
 using namespace std;
 using namespace cv;
 
-vector<vector<Point>> contour_filtering(vector<vector<Point>> contour, int min_conut, double height_to_width_ratio, double percentage_error);
+vector<vector<Point>> contour_filtering(vector<vector<Point>> contour, pair<int, int> conut, double height_to_width_ratio, double percentage_error);
 Mat image_binary_processing(Mat image, int thres);
 bool sort_by_size(pair<int, int> const& lhs, pair<int, int> const& rhs);
 
 int main(){
     VideoCapture cap("10.1_Assignment_video_clip.mp4");
-    Mat imgFrame, image_copied, image_binary;
+    Mat imgFrame, image_copied, image_binary, image_processed;
     vector<vector<Point>> contour;
     vector<Vec4i> hierarchy;
     if(!cap.isOpened()){
@@ -35,10 +35,22 @@ int main(){
     char chCheckForEscKey = 0;
     while (cap.isOpened() && chCheckForEscKey != 27){
         imshow("imgFrame", imgFrame);
+        cout << "showed";
         imgFrame.copyTo(image_copied);
-        image_binary = image_binary_processing(image_copied, 200);
-        findContours(image_binary, contour, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0,0));
-        contour_filtering(contour, 10, 2, 0.3);
+        image_binary = image_binary_processing(image_copied, 210);
+        findContours(image_binary, contour, hierarchy,  CV_RETR_EXTERNAL,CV_CHAIN_APPROX_SIMPLE, Point(0,0));
+       
+        //draw contour
+        for (int i = 0; i < contour.size(); i++){
+            drawContours(image_copied, contour, i, Scalar(0, 0,255), 3, LINE_8, hierarchy, 0, Point());
+        }
+        contour = contour_filtering(contour, make_pair(10, 220), 1.5, 0.3);
+        
+        for (int i = 0; i < contour.size(); i++){
+            drawContours(image_copied, contour, i, Scalar(0, 255, 0), 3, LINE_8, hierarchy, 0, Point());
+        }
+        
+        image_processed = image_copied;
         if((cap.get(CV_CAP_PROP_POS_FRAMES) + 1) < cap.get(CV_CAP_PROP_FRAME_COUNT)){
             cap.read(imgFrame);
         }
@@ -50,8 +62,8 @@ int main(){
         }
 
         //show the processed frame
-        imshow("Processed", image_binary);
-        waitKey(30);
+        imshow("Processed", image_processed);
+        chCheckForEscKey = waitKey(100);
     }
 
     if (chCheckForEscKey != 27){
@@ -67,38 +79,62 @@ Mat image_binary_processing(Mat image, int thres){
     return image_binary;
 }
 
-vector<vector<Point>> contour_filtering(vector<vector<Point>> contour, int min_count, double height_to_width_ratio, double percentage_error){
+vector<vector<Point>> contour_filtering(vector<vector<Point>> contour, pair<int, int> count, double height_to_width_ratio, double percentage_error){
    //filter for the useless small porint first
     int index = 0;
     vector<pair<int ,int>> mapping_sizeToIndex;
+    vector<vector<Point>> filtered_contour;
     for (vector<vector<Point>>::iterator it = contour.begin(); it != contour.end(); index++){
-        // cout << "contour " << index << "(" <<  (*it)[0].x << " ," << (*it)[0].y << "), number of points:" << (*it).size() << endl;
-        if ((*it).size() < min_count){
-            /*cout << "erased " << endl;*/
+        //cout << "contour " << index << "(" <<  (*it)[0].x << " ," << (*it)[0].y << "), number of points:" << (*it).size() << endl;
+        Rect rect = boundingRect((*it)); 
+        Point top_left_corner = rect.tl();
+        Point buttom_right_corner = rect.br() - Point(1, 1);
+        double rect_length = buttom_right_corner.x - top_left_corner.x;
+        double rect_width = buttom_right_corner.y - top_left_corner.y;
+        double rect_scale = rect_length/rect_width;
+        double error = abs(rect_scale - height_to_width_ratio)/ height_to_width_ratio;
+        if (!((*it).size() > count.first && (*it).size() < count.second) || error > percentage_error){
+            //cout << "erased " << endl;
             it = contour.erase(it);
             
         }
+        else{
+           //cout << "passed" << endl;
+           ++it;
+        }
+       //cin.get(); i
     }
-           
+        
+    
+    //cout << "passed first filtering";  
     // after erasing the invalid count, try to calculate its dimension, size and ratio
-    for (vector<vector<Point>>::iterator it = contour.begin(); it != contour.end(); index++, it++){       
+    index = 0;
+    for (vector<vector<Point>>::iterator it = contour.begin(); it != contour.end(); index++, ++it){       
         Rect rect = boundingRect((*it));
-            
         Point top_left_corner = rect.tl();
         Point buttom_right_corner = rect.br() - Point(1, 1);
             
-        cout << "(" << top_left_corner.x << ',' << top_left_corner.y << ") (" << buttom_right_corner.x << ", " << buttom_right_corner.y << ")" << endl;
         double rect_length = buttom_right_corner.x - top_left_corner.x;
         double rect_width = buttom_right_corner.y - top_left_corner.y;
+        //cout << "(" << rect_length << ',' << rect_width << ")";
         int rect_area = rect_length * rect_width;
+        //cout << "area of rect " << index << ": " <<  rect_area << endl;
+        // cin.get();
         mapping_sizeToIndex.push_back(make_pair(rect_area, index));
         
     }
     // sort them by size, largest come first
     sort(mapping_sizeToIndex.begin(), mapping_sizeToIndex.end(), sort_by_size);
-    contour.erase(contour.begin()+9, contour.end());
+    
+    for(int i = 0; i < 9;i++){
+        // assign the largest 9 contour to the filtered_contour
+       /* cout << "assigned rect" << mapping_sizeToIndex[i].second << "with area" << mapping_sizeToIndex[i].first << endl;*/
+        filtered_contour.push_back(contour[mapping_sizeToIndex[i].second]);
+        cout << mapping_sizeToIndex[i].first << " " << mapping_sizeToIndex[i].second << endl;
+    }
+
     cout << "finsihed";
-    return contour;
+    return filtered_contour;
 }
 
 bool sort_by_size(pair<int, int> const& lhs, pair<int, int> const& rhs){
