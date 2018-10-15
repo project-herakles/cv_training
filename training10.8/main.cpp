1cd5c21e9544a42f524efd81b19da650151626c5
neoys97 <neoys97@gmail.com>
2018-10-15 12:28:56 +0800

    completed main.cpp comments

--- training10.8/main.cpp
+++ training10.8/main.cpp
@@ -4,25 +4,31 @@
 
 using namespace std;
 using namespace cv;
-
+//Neo - define the kernel (size of matrix) used for dilation of the image
 Mat contourThreadkernel = getStructuringElement(MORPH_ELLIPSE, Size(9,9));
-
+//Neo - find the contour of the input image
 void ContourThread(int mode,Mat input,Mat &binary,int thres,vector<vector<Point> > &contours)
 {
     Mat thres_whole;
     vector<Mat> splited;
+    //Neo - split the input image into 3 color channels namely Blue, Green and Red
     split(input,splited);
     cvtColor(input,thres_whole,CV_BGR2GRAY);
     threshold(thres_whole,thres_whole,100,255,THRESH_BINARY);
     if(mode == 0){
+	//for finding red target, substract the blue channel from the red channel (for clearer contours)
         subtract(splited[2],splited[0],binary);
         threshold(binary,binary,thres,255,THRESH_BINARY);
     }else{
+	//for finding blue target, substract the red channel from the blue channel (for clearer contours)
         subtract(splited[0],splited[2],binary);
         threshold(binary,binary,thres,255,THRESH_BINARY);
     }
+    //Neo - dilate the binary image with the kernel size specified above (dilation increases the size of a white area of the binary image)
     dilate(binary,binary,contourThreadkernel);
+    //Neo - combining the dilated binary image with the initial threshold processed image to get the details of the possible light bar parts
     binary = binary & thres_whole; 
+    //Neo - find the contour of the final processed binary image
     findContours(binary,contours,CV_RETR_TREE,CV_CHAIN_APPROX_SIMPLE);
 }
 
