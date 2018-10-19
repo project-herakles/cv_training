79b21d48b2c8eff792f60a21aaa91ec592035a70
GloRiation <ivanyiu2017@gmail.com>
2018-10-19 21:41:40 +0800

    partially comment on the armorpredict.h, armorpredict.cpp, StereoXML.cpp

---
+++ assignment_3/src/StereoXML.cpp
@@ -0,0 +1,42 @@
+#include "include/Camera/StereoXML.h"
+
+/**
+  * @brief calculate the world's corrdinate of matching points from left and right camera
+  * @param left/right: left and right points
+  * @param Position  : the corrdinate output
+  * @return none
+  */
+
+// Ivan - calcualte the x-distance between two point, z-position(camera to object distance)
+//        x-position, y-position, which is the world coordinate???
+void StereoXML::Calculation(const Point &left,const Point &right,AbsPosition &Position){
+    //std::cout<<"left y position:"<<left.y<<std::endl;
+    VisionDis = left.x - right.x;
+    Position.z = Param.distance_const/VisionDis;
+    Position.x = (left.x - Param.width/2)*Position.z/Param.f;
+    Position.y = (Param.lightbase- left.y)*Position.z/Param.f;
+}
+
+StereoXML::StereoXML(){
+
+}
+
+StereoXML::StereoXML(const string &xmlfile){
+    FileStorage fs(xmlfile,FileStorage::READ);
+    fs["height"]>>Param.height;
+    fs["width"]>>Param.width;
+    fs["distance_const"]>>Param.distance_const;
+    fs["f"]>>Param.f;
+    fs["lightbase"]>>Param.lightbase;
+    fs.release();
+}
+
+void StereoXML::Init(const string &xmlfile){
+    FileStorage fs(xmlfile,FileStorage::READ);
+    fs["height"]>>Param.height;
+    fs["width"]>>Param.width;
+    fs["distance_const"]>>Param.distance_const;
+    fs["f"]>>Param.f;
+    fs["lightbase"]>>Param.lightbase;
+    fs.release();
+}
--- assignment_3/src/armorpredict.cpp
+++ assignment_3/src/armorpredict.cpp
@@ -6,6 +6,7 @@ ArmorPredict::ArmorPredict()
 }
 
 ArmorPredict::ArmorPredict(const string &File640,const string &File1280,const string &File1920,InitParams param){
+    // Ivan - set the parameters by reading the files
     memset(&Result,0,sizeof(AbsPosition));
     Stereo640.Init(File640);
     Stereo1280.Init(File1280);
@@ -49,6 +50,7 @@ void ArmorPredict::PredictMono(vector<Monodata> &mono_input,int videotype,double
         Data.pitch_angle.d = 0;
         return;
     }
+    // Ivan - check if the execution time is normal
     if(time != 0){
         T_last = T_curr;
         T_curr = time;
@@ -57,6 +59,7 @@ void ArmorPredict::PredictMono(vector<Monodata> &mono_input,int videotype,double
     }
     int size = mono_input.size();
     for(int i=0;i<size;i++){
+        // Ivan calculate the distance of the aromor according to the resolution
         if(mono_input[i].armor == mono_input[i].small_armor){
             if(videotype == 1280){
                 mono_input[i].distance = sqrtf(mono_small_armor_720p_const/mono_input[i].area);
@@ -76,6 +79,9 @@ void ArmorPredict::PredictMono(vector<Monodata> &mono_input,int videotype,double
                                                 });
     //std::cout<<"distance:"<<mono_input[0].distance<<std::endl;
     float cal_x,cal_y;
+    // Ivan - calculate the corrected x,y position of the armor according to the resolution
+    //        640 is the half of 1280
+    //        960 is the half of 1920
     if(videotype == 1280){
         cal_x = (mono_input[0].center.x-640)*mono_input[0].distance/mono_f_720p;
         cal_y = (mono_base_720p-mono_input[0].center.y)*mono_input[0].distance/mono_f_720p;
@@ -84,18 +90,25 @@ void ArmorPredict::PredictMono(vector<Monodata> &mono_input,int videotype,double
         cal_y = (mono_base_1080p-mono_input[0].center.y)*mono_input[0].distance/mono_f_1080p;
     }
     //std::cout<<"x:"<<cal_x<<" y:"<<cal_y<<std::endl;
+    // Ivan - calculate the relative mono xyz position with respect to the corrected position of the armor
     float last_x = mono_Pos.x;
     mono_Pos.x = cal_x - mono_x;
     mono_Pos.y = cal_y - mono_y;
     mono_Pos.z = mono_input[0].distance - mono_z;
     if(mono_Pos.z <= 0) return;
+    // Ivan - calculate the yaw angle with the relative position x,z(distance) with this formula
+    //        if the execution time is normal and last yaw_angle is not 0
+    //        then calculate also the yaw_speed.d by the difference in yaw_angle compared with the past data
     int16_t yangle = (int16_t)(-atan(mono_Pos.x/mono_Pos.z)*1303.7972938);
     Data.yaw_speed.d = 0;
     if(T_between != 0 && time != 0 && Data.yaw_angle.d != 0){
         Data.yaw_speed.d = yangle - Data.yaw_angle.d;
+        // Ivan - if the changes is small, the yaw_speed.d = 0
          if(abs(last_x - mono_Pos.x) < 4) Data.yaw_speed.d = 0;
     }
+    // Ivan - update the datas
     Data.yaw_angle.d = yangle;
+    // Ivan - Calculate the pitch angle with the help of y, z(distance) with this formula
     Data.pitch_angle.d =(int16_t)(atan(mono_Pos.y/mono_Pos.z)*1303.7972938);
     Data.shoot_speed.f = 0;
     Data.z.f = mono_input[0].distance;
@@ -115,10 +128,12 @@ void ArmorPredict::PredictMono(vector<Monodata> &mono_input,int videotype,double
   */
 void ArmorPredict::Predict(vector<Point> Left, vector<Point> Right,int videotype,double time,int robotlevel){
     Fresh();
+    // Ivan - execution time check
     T_last = T_curr;
     T_curr = time;
     T_between = (double)(T_curr - T_last) / getTickFrequency();
     if(T_between > 0.5) T_between = 0;
+    // Ivan - put the coroect yaw_speed based on which camera detects things
     if(Left.size() == 0 && Right.size() == 0){
         yaw_out = 0;
         pitch_out = 0;
@@ -151,18 +166,23 @@ void ArmorPredict::Predict(vector<Point> Left, vector<Point> Right,int videotype
             memset(&Data,0,sizeof(VisionData));
         }
     }else{
+        // Ivan - its case for both camera detects things
         auto sort_point = [](const Point &a1,const Point &a2){
             return a1.x < a2.x;
           };
+        // Ivan - sort both vector with the x pos of the points
         std::sort(Left.begin(),Left.end(),sort_point);
         std::sort(Right.begin(),Right.end(),sort_point);
         size_t Lsize = Left.size();
         size_t Rsize = Right.size();
         for(int i=0,j=0;i<Lsize,j<Rsize;i++,j++){
             if(Left[i].x > Right[j].x){
+                // Ivan - do the following if the condition is met, but why?????
                 if(videotype == 1920){
                     AbsPosition Pos;
                     Stereo1920.Calculation(Left[i],Right[j],Pos);
+                    // Ivan - if the distance  > 30(Minimum valild distance?) , correct the position with
+                    //        transxyz(I dont know where is it come from) and store it in the Positions
                     if(Pos.z > 30){
                         Pos.x -= x_trans;
                         Pos.y -= y_trans;
@@ -190,19 +210,23 @@ void ArmorPredict::Predict(vector<Point> Left, vector<Point> Right,int videotype
             }
         }
     }
+    // Ivan - sort the Positions according to their distance to camera if needed 
     if(Positions.size() != 0){
         if(Positions.size() != 1){
             sort(Positions.begin(),Positions.end(),PosSort);
         }
         if(Positions.size() > 1){
+            // Ivan - if the distance difference < 100
             if(fabs(Positions[0].z - Positions[1].z) < 100 && OldResult.z != 0){
                 float q0 = fabs(OldResult.x - Positions[0].x);
                 float q1 = fabs(OldResult.x - Positions[1].x);
                 Result = q0 < q1 ? Positions[0] : Positions[1];
             }else{
+                // Ivan - if its valid, sotre the position of nearest armor to reulst
                 Result = Positions[0];
             }
         }else{
+            // Ivan - store the position to the only armor into result 
             Result = Positions[0];
         }
 
@@ -214,20 +238,25 @@ void ArmorPredict::Predict(vector<Point> Left, vector<Point> Right,int videotype
         Data.z.f = Result.z;
         Leftcenter = Left[Result.index];
         Rightcenter = Right[Result.index];
+        // Ivan - dont do anything if the result is too close (<30)
         if(Result.z < 30){
             yaw_out = 0;
             pitch_out = 0;
         }else{
+            // Ivan - if its in suitable distance, execute this function
             AngleFit(Result,&pitch_out,&yaw_out,&shoot_speed,robotlevel);
         }
+        // Ivan - prevent yaw_out and pitch_out > 500
         if(fabs(yaw_out) > 500 ||fabs(pitch_out) > 500) {
             yaw_out = yaw_old;
             pitch_out = pitch_old;
         }
+        // Ivan - If the armor position difference is too large(>1000), dont move the yaw(set speed it as 0)
         if(fabs(OldResult.x - Result.x) > 1000){
             yaw_out_speed = 0;
         }else{
             if(yaw_out != 0.0 && yaw_old != 0.0){
+                // Ivan - speed = distance / time
                 yaw_out_speed = (yaw_out - yaw_old)/T_between;//encoder/s
             }
         }
@@ -286,6 +315,7 @@ void ArmorPredict::Classical_kalman(Kalman4Point &KF){
   */
 void ArmorPredict::AngleFit(const AbsPosition input,float *pitch,float *yaw,float *shootspd,int level){
     float flytime,gravity_offset;
+    // Ivan - Calculate the Educean distance in 3d, express in temrs of meter 
     float shoot_distance = sqrtf(input.x * input.x + input.y * input.y + input.z * input.z)*0.001; // m
     // expect bullet fly time is 0.1s
     *shootspd = shoot_distance * 10.0;//
