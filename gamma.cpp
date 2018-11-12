3b514eaecf253beb5a82671eddfaff7e57df3996
linyaya <43580250+linyaya@users.noreply.github.com>
2018-11-12 17:48:55 +0800

    Create gamma.cpp

---
+++ gamma.cpp
@@ -0,0 +1,13 @@
+void gamma(Mat& image){
+	image.convertTo(image , CvType.CV_32FC3);
+	for (int i = 0; i < image.rows; i++)
+	{
+		for (int j = 0; j < image.cols; j++)
+		{
+			image.at<Vec3f>(i, j)[0] = (image.at<Vec3b>(i, j)[0])*(image.at<Vec3b>(i, j)[0])*(image.at<Vec3b>(i, j)[0]);
+			image.at<Vec3f>(i, j)[1] = (image.at<Vec3b>(i, j)[1])*(image.at<Vec3b>(i, j)[1])*(image.at<Vec3b>(i, j)[1]);
+			image.at<Vec3f>(i, j)[2] = (image.at<Vec3b>(i, j)[2])*(image.at<Vec3b>(i, j)[2])*(image.at<Vec3b>(i, j)[2]);
+		}
+	}
+	normalize(image, image, 0, 255, CV_MINMAX, CvType.CV_8UC3);
+}
